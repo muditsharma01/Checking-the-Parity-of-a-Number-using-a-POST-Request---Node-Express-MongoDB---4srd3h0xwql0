1379c082f05227700e1724267c305f86702dda46
Mudit Sharma <muditshar001@gmail.com>
2023-11-19 21:24:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,16 +8,37 @@ const server = http.createServer((req, res) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value = obj.num1;
-    
-     // Write the code here to check if the number is odd or even
+    });
 
-   });
+    req.on('end', () => {
+      try {
+        const data = chunks.join('');
+        const obj = JSON.parse(data);
+
+        if (!obj.num1 || typeof obj.num1 !== 'number') {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid request. Please provide a valid JSON payload with a single integer field "num1".');
+          return;
+        }
+
+        const num1 = obj.num1;
+
+        if (num1 % 2 === 0) {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`The number ${num1} is even`);
+        } else {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end(`The number ${num1} is odd`);
+        }
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON format in the request body.');
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Invalid endpoint. Only POST requests are accepted at the root ("/").');
   }
-
-  
 });
-
 
 module.exports = server;
